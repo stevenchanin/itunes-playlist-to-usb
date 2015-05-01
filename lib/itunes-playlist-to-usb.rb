e0a90a78ae9d683bb5fd2fc7e2740eaedd66aefa
Jon Higgs <jhiggs@eml.cc>
2015-05-01 17:36:35 +1000

    Move the settings code.

--- lib/itunes-playlist-to-usb.rb
+++ lib/itunes-playlist-to-usb.rb
@@ -1,8 +1,19 @@
 $:.unshift File.dirname(__FILE__)
 $:.unshift File.join(File.dirname(__FILE__), '/itunes-playlist-to-usb')
 
+
 require 'byebug'
 require 'yaml'
+
+### LOAD SETTINGS #############################################################
+settings_paths = [
+  File.join(File.dirname(__FILE__), "../etc/settings.yml"),
+  File.expand_path("~/.itunes-playlist-to-usb"),
+]
+
+settings_paths.each { |s| SETTINGS = YAML.load_file(s) if File.exist?(s) }
+
+#### PROGRESS BAR #############################################################
 
 require 'convert'
 require 'playlist_manager'
