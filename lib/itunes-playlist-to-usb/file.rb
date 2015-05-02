a946e7ea4945d1b6210a1a7ceb9c0de53395b838
Jon Higgs <jhiggs@eml.cc>
2015-05-02 20:13:48 +1000

    Speed improvements for File.

--- lib/itunes-playlist-to-usb/file.rb
+++ lib/itunes-playlist-to-usb/file.rb
@@ -1,30 +1,15 @@
 module PL2USB
   require 'audioinfo'
   class File
-    attr_reader :length
     attr_reader :extension
-    attr_reader :size
     attr_reader :path
     attr_reader :kind
-    attr_reader :bitrate
 
     def initialize path, kind
       @path = path
       @kind = kind
       @codec_info = YAML.load_file(::File.join(::File.dirname(__FILE__), "../../etc/codecs.yml"))[@kind]
       @extension = @codec_info["extension"]
-
-      begin
-        @info = ::AudioInfo.new(path)
-        @length = @info.length
-        @size = ::File.size(path)
-        @bitrate = @info.bitrate
-      rescue
-        @info = nil
-        @length = nil
-        @size = nil
-        @bitrate = nil
-      end
     end
 
     def lossless?
@@ -34,5 +19,26 @@ module PL2USB
     def exist?
       ::File.exist? path
     end
+
+    def size
+      exist? ? ::File.size(path) : nil
+    end
+
+    def length
+      begin
+        ::AudioInfo.new(path).length
+      rescue
+        nil
+      end
+    end
+
+    def bitrate
+      begin
+        ::AudioInfo.new(path).bitrate
+      rescue
+        nil
+      end
+    end
+
   end
 end
