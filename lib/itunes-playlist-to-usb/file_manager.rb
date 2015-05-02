7a56f6f44a056399861a96cb52926d1b66155bd7
Jon Higgs <jhiggs@eml.cc>
2015-05-02 20:14:32 +1000

    Delete the FileManager for now.

--- lib/itunes-playlist-to-usb/file_manager.rb
+++
@@ -1,8 +0,0 @@
-class FileManager
-  def initialize
-    # - have a library with each of the different converted versions.
-    # - add an rsync tool to export the specific files to a usb key.
-    # - remove out of date files from the library when they are changed.
-  end
-
-end
