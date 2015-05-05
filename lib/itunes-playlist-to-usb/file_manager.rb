76ff66291eea4e407a42847dbd05500bb2490074
Jon Higgs <jhiggs@eml.cc>
2015-05-05 17:38:25 +1000

    Clean up orphans from the library directory.

--- lib/itunes-playlist-to-usb/file_manager.rb
+++ lib/itunes-playlist-to-usb/file_manager.rb
@@ -12,12 +12,37 @@ module PL2USB
       existing - expected
     end
 
+    def orphaned_directories
+      ::Dir.glob("#{library}**/*").delete_if do |p|
+        ::File.file?(p) || ::Dir.entries(p).count != 2
+      end
+    end
+
     def expected
       @playlist.tracks.map{|t| t.destination.path }
     end
 
     def existing
       ::Dir.glob("#{library}**/*").map{|p| p if ::File::file?(p) }.compact
+    end
+
+    def delete_orphaned!
+      delete_orphaned_files!
+      delete_orphaned_directories!
+    end
+
+    def delete_orphaned_files!
+      orphaned.each do |orphan|
+        PROGRESS_BAR.debug_log("deleting orphaned file '#{orphan}'.")
+        ::File.unlink(orphan)
+      end
+    end
+
+    def delete_orphaned_directories!
+      orphaned_directories.each do |directory|
+        PROGRESS_BAR.debug_log("deleting empty directory '#{directory}'.")
+        ::Dir.delete(directory)
+      end
     end
 
     def library
--- spec/file_manager_spec.rb
+++ spec/file_manager_spec.rb
@@ -11,18 +11,32 @@ RSpec.describe PL2USB::FileManager do
       expect(file_manager.orphaned).to be_empty
     end
 
+    it "should not find any orphaned directories" do
+      ::FileUtils.rm_rf(file_manager.library)
+      expect(file_manager.orphaned_directories).to be_empty
+    end
+
     it "should not find any existing files" do
       ::FileUtils.rm_rf(file_manager.library)
       expect(file_manager.existing).to be_empty
     end
   end
 
-  context "with orphaned file" do
+  context "with orphans" do
     file_manager = PL2USB::FileManager.new(PL2USB::Playlist.new(PLAYLIST_XML))
     it "should find orphaned file" do
       ::FileUtils.mkdir_p(file_manager.library)
       ::FileUtils.touch(::File.join(file_manager.library, "orphan.mp3"))
       expect(file_manager.orphaned).to eql ["/tmp/library/orphan.mp3"]
+      file_manager.delete_orphaned_files!
+      expect(file_manager.orphaned).to be_empty
+    end
+
+    it "should find orphaned directory" do
+      ::FileUtils.mkdir_p(file_manager.library+"/orphaned_directory")
+      expect(file_manager.orphaned_directories).to eql ["/tmp/library/orphaned_directory"]
+      file_manager.delete_orphaned_directories!
+      expect(file_manager.orphaned_directories).to be_empty
     end
 
     it "should not found any existing files" do
